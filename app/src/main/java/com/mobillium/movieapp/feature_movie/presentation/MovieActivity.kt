d3d4d966d6dff04a7077a45cfa5ccb0cefc0346d
Batuhan Güneş <batuhangunes001@gmail.com>
2023-04-15 16:30:10 +0300

    Inject usecases into view model class

--- app/src/main/java/com/mobillium/movieapp/feature_movie/presentation/MovieActivity.kt
+++ app/src/main/java/com/mobillium/movieapp/feature_movie/presentation/MovieActivity.kt
@@ -1,10 +1,16 @@
 package com.mobillium.movieapp.feature_movie.presentation
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.mobillium.movieapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MovieActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MovieActivity : AppCompatActivity() {
+
+    private val viewModel: MovieActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
