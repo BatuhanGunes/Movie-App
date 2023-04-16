bffed76a87655bfeb44b3bc397fc84a23e745b84
Batuhan Güneş <batuhangunes001@gmail.com>
2023-04-16 08:49:43 +0300

    Add Glide library and draw images from remote

--- app/src/main/java/com/mobillium/movieapp/feature_movie/presentation/movie_list/adapters/MovieRecyclerViewAdapter.kt
+++ app/src/main/java/com/mobillium/movieapp/feature_movie/presentation/movie_list/adapters/MovieRecyclerViewAdapter.kt
@@ -1,12 +1,16 @@
 package com.mobillium.movieapp.feature_movie.presentation.movie_list.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.mobillium.movieapp.core.utils.EndPoints
 import com.mobillium.movieapp.databinding.CardViewMovieItemBinding
 import com.mobillium.movieapp.feature_movie.domain.entity.movie.MovieEntity
+import com.mobillium.movieapp.feature_movie.presentation.common.extension.clearImageWithGlide
+import com.mobillium.movieapp.feature_movie.presentation.common.extension.loadImageFromURL
 import com.mobillium.movieapp.feature_movie.presentation.movie_list.ListMovieFragmentDirections
 
 class MovieRecyclerViewAdapter :
@@ -15,13 +19,22 @@ class MovieRecyclerViewAdapter :
     inner class MovieViewHolder(private val itemBinding: CardViewMovieItemBinding) :
         RecyclerView.ViewHolder(itemBinding.root) {
         fun bind(movie: MovieEntity) {
-            //itemBinding.itemMovieImage = movie.posterPath todo
             itemBinding.movieItemTitle.text = movie.title
             itemBinding.movieItemDescription.text = movie.overview
             itemBinding.movieItemUpdateDate.text = movie.releaseDate
+
+            val rootContext: Context = itemBinding.root.context
+            if (movie.posterPath.isNotEmpty()) {
+                val basePosterPath = EndPoints.getBasePosterPath(movie.posterPath)
+                rootContext.loadImageFromURL(itemBinding.itemMovieImage, basePosterPath)
+            } else {
+                rootContext.clearImageWithGlide(itemBinding.itemMovieImage)
+            }
+
             itemBinding.root.setOnClickListener {
                 val movieId: String = movie.id.toString()
-                val action = ListMovieFragmentDirections.actionListMovieFragmentToDetailMovieFragment(movieId)
+                val action =
+                    ListMovieFragmentDirections.actionListMovieFragmentToDetailMovieFragment(movieId)
                 Navigation.findNavController(it).navigate(action)
             }
         }
