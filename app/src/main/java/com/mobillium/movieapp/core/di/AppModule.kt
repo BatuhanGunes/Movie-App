84e83d79b8cd395da4ca3ec549da337f336cca92
Batuhan Güneş <batuhangunes001@gmail.com>
2023-04-16 03:44:13 +0300

    Refactor: renaming entity and removing unnecessary use case

--- app/src/main/java/com/mobillium/movieapp/core/di/AppModule.kt
+++ app/src/main/java/com/mobillium/movieapp/core/di/AppModule.kt
@@ -6,7 +6,6 @@ import com.mobillium.movieapp.feature_movie.domain.MovieRepository
 import com.mobillium.movieapp.feature_movie.domain.use_case.GetMovieDetail
 import com.mobillium.movieapp.feature_movie.domain.use_case.GetNowPlayingMovies
 import com.mobillium.movieapp.feature_movie.domain.use_case.GetUpcomingMovies
-import com.mobillium.movieapp.feature_movie.domain.use_case.MovieUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,17 +18,20 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideMovieRepository(movieService: MovieService) : MovieRepository {
+    fun provideMovieRepository(movieService: MovieService): MovieRepository {
         return MovieRepositoryImpl(movieService)
     }
 
     @Provides
     @Singleton
-    fun provideNoteUseCases(repository: MovieRepository): MovieUseCases {
-        return MovieUseCases(
-            GetMovieDetail(repository),
-            GetUpcomingMovies(repository),
-            GetNowPlayingMovies(repository),
-        )
-    }
+    fun provideGetMovieDetailUseCases(repository: MovieRepository) = GetMovieDetail(repository)
+
+    @Provides
+    @Singleton
+    fun provideGetNowPlayingMoviesUseCases(repository: MovieRepository) = GetNowPlayingMovies(repository)
+
+    @Provides
+    @Singleton
+    fun provideGetUpcomingMoviesUseCases(repository: MovieRepository) = GetUpcomingMovies(repository)
+
 }
