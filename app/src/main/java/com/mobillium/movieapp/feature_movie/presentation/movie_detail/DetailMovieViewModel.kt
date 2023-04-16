19b0760d523916ec670c1ae8bace2270cf9512a6
Batuhan Güneş <batuhangunes001@gmail.com>
2023-04-16 04:02:10 +0300

    Perform movieDetailUIState operations and collect data from repositories

--- app/src/main/java/com/mobillium/movieapp/feature_movie/presentation/movie_detail/DetailMovieViewModel.kt
+++ app/src/main/java/com/mobillium/movieapp/feature_movie/presentation/movie_detail/DetailMovieViewModel.kt
@@ -1,11 +1,16 @@
 package com.mobillium.movieapp.feature_movie.presentation.movie_detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mobillium.movieapp.feature_movie.domain.common.base.BaseResult
 import com.mobillium.movieapp.feature_movie.domain.entity.movie_details.MovieDetailEntity
 import com.mobillium.movieapp.feature_movie.domain.use_case.GetMovieDetail
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,6 +19,37 @@ class DetailMovieViewModel @Inject constructor(private val getMovieDetail: GetMo
     private val _state = MutableStateFlow<MovieDetailFragmentUIState>(MovieDetailFragmentUIState.Init)
     val state: StateFlow<MovieDetailFragmentUIState> = _state
 
+    private fun setLoading() {
+        _state.value = MovieDetailFragmentUIState.IsLoading(true)
+    }
+
+    private fun hideLoading() {
+        _state.value = MovieDetailFragmentUIState.IsLoading(false)
+    }
+
+    private fun showToast(message: String) {
+        _state.value = MovieDetailFragmentUIState.ShowToast(message)
+    }
+
+    fun getMovieDetail(movieId: String) {
+        viewModelScope.launch {
+            getMovieDetail.invoke(movieId = movieId)
+                .onStart {
+                    setLoading()
+                }
+                .catch { exception ->
+                    hideLoading()
+                    showToast(exception.message.toString())
+                }
+                .collect { baseResult ->
+                    hideLoading()
+                    when (baseResult) {
+                        is BaseResult.Error -> _state.value = MovieDetailFragmentUIState.Error(baseResult.message, baseResult.errorCode)
+                        is BaseResult.Success -> _state.value = MovieDetailFragmentUIState.SuccessMovieDetailDetail(baseResult.data)
+                    }
+                }
+        }
+    }
 }
 
 sealed class MovieDetailFragmentUIState {
